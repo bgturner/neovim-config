2964fb20293054d17a1e1cc50f293d96ceb3bdf9
Benjamin Turner <benjamin@passionsplay.com>
2023-09-28 21:10:17 -0700

    feat: add copilot.lua

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "InsertEnter" },
+    cmd = { "Copilot" },
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+      },
+    },
+  },
+}
