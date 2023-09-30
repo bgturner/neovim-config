c29d0d88fac32f3e1aad78316c6861664699a2a1
Benjamin Turner <benjamin@passionsplay.com>
2023-09-29 20:52:15 -0700

    feat: add additional text objects

---
+++ lua/plugins/text-object.lua
@@ -0,0 +1,10 @@
+return {
+  -- Adds text objects such as vii.
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    event = "VeryLazy",
+    config = function()
+      require("various-textobjs").setup({ useDefaultKeymaps = true })
+    end,
+  },
+}
