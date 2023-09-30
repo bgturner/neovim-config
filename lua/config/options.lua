eccfbff5aa0e05abf1172ac5f28365d33fa5771a
Benjamin Turner <benjamin@passionsplay.com>
2023-09-29 21:55:14 -0700

    feat: better visualization of whitespace

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,11 @@
 --
 --
 vim.opt.relativenumber = false
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↲")
+vim.opt.listchars:append("tab:→ ")
+vim.opt.listchars:append("extends:⟩")
+vim.opt.listchars:append("precedes:⟨")
+vim.opt.listchars:append("nbsp:␣")
